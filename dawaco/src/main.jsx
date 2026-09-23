de131d73ddc93a88445f97fe8af05d13466cd3b0
Sonu Patil <prernapatil7798@gmail.com>
2026-09-23 13:35:43 +0530

    Fix React Router for GitHub Pages

--- dawaco/src/main.jsx
+++ dawaco/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
 import "./index.css";
@@ -9,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/davaco">
       <AuthProvider>
         <CartProvider>
           <App />
